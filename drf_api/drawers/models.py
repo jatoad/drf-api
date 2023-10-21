c1560d67b76294ad6efba4f932f5807ea41dc1a9
jatoad <jasperhmca.todd@gmail.com>
2023-10-21 18:07:48 +0000

    add drawer and supporting framework

---
+++ drf_api/drawers/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Drawer(models.Model):
+    """
+    Drawer model, related to 'owner', i.e. a User instance.
+    Default image set so that we can always reference image.url.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    desciption = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_profile_mx0cj', blank=True
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
