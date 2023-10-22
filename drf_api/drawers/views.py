6f45f4c6fade96ecfc4d26b8711d99bf8047d816
jatoad <jasperhmca.todd@gmail.com>
2023-10-22 09:28:53 +0000

    added drawers and profiles generic views

--- drf_api/drawers/views.py
+++ drf_api/drawers/views.py
@@ -1,71 +1,26 @@
-from django.http import Http404
-from rest_framework import status, permissions
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Drawer
 from .serializers import DrawerSerializer
-from drf_api.permissions import IsOwnerOrReadOnly
 
-class DrawerList(APIView):
+
+class DrawerList(generics.ListCreateAPIView):
+    """
+    List drawers or create a drawer if logged in
+    The perform_create method associates the post with the logged in user.
+    """
     serializer_class = DrawerSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
-    ]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Drawer.objects.all()
 
-    def get(self, request):
-        drawers = Drawer.objects.all()
-        serializer = DrawerSerializer(
-            drawers, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
-    def put(self, request):
-        serializer = DrawerSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
-    
-class DrawerDetail(APIView):
+
+class DrawerDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a post and edit or delete it if you own it.
+    """
+    serializer_class = DrawerSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    serializer_class = DrawerSerializer
-
-    def get_object(self, pk):
-        try:
-            drawer = Drawer.objects.get(pk=pk)
-            self.check_object_permissions(self.request, drawer)
-            return drawer
-        except Drawer.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        drawer = self.get_object(pk)
-        serializer = DrawerSerializer(
-            drawer, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        drawer = self.get_object(pk)
-        serializer = DrawerSerializer(
-            drawer, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
-
-    def delete(self, request, pk):
-        drawer = self.get_object(pk)
-        drawer.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+    queryset = Drawer.objects.all()
--- drf_api/profiles/views.py
+++ drf_api/profiles/views.py
@@ -1,36 +1,22 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics
+from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
 
 
-class ProfileList(APIView):
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True)
-        return Response(serializer.data)
-
-class ProfileDetail(APIView):
+class ProfileList(generics.ListAPIView):
+    """
+    List all profiles.
+    No create view as profile creation is handled by django signals.
+    """
+    queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
-    
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
-        
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile)
-        return Response(serializer.data)
-    
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+
+
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve or update a profile if you're the owner.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
