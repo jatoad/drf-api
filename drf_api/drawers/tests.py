e3864361e0d799d2b8122c7a9c72d13f95b2c1a2
jatoad <jasperhmca.todd@gmail.com>
2023-10-22 12:03:16 +0000

    added tests for items

--- drf_api/drawers/tests.py
+++ drf_api/drawers/tests.py
@@ -6,20 +6,20 @@ from rest_framework.test import APITestCase
 
 class DrawerListViewTests(APITestCase):
     def setUp(self):
-        User.objects.create_user(username='adam', password='pass')
+        User.objects.create_user(username='chris', password='pass')
 
     def test_can_list_drawers(self):
-        adam = User.objects.get(username='adam')
-        Drawer.objects.create(owner=adam, title='a title')
+        chris = User.objects.get(username='chris')
+        Drawer.objects.create(owner=chris, title='a title')
         response = self.client.get('/drawers/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         print(response.data)
         print(len(response.data))
 
 #     def test_logged_in_user_can_create_drawer(self):
-#         self.client.login(username='adam', password='pass')
+#         self.client.login(username='chris', password='pass')
 #         response = self.client.post('/drawers/', {'title': 'a title'})
-#         count = Drawer.objects.count()e
+#         count = Drawer.objects.count()
 #         self.assertEqual(count, 1)  
 #         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
@@ -29,13 +29,13 @@ class DrawerListViewTests(APITestCase):
 
 class DrawerDetailViewTests(APITestCase):
     def setUp(self):
-        adam = User.objects.create_user(username='adam', password='pass')
-        brian = User.objects.create_user(username='brian', password='pass')
+        chris = User.objects.create_user(username='chris', password='pass')
+        fred = User.objects.create_user(username='fred', password='pass')
         Drawer.objects.create(
-            owner=adam, title='a title', description='adams content'
+            owner=chris, title='a title', description='Chris content'
         )
         Drawer.objects.create(
-            owner=brian, title='another title', description='brians content'
+            owner=fred, title='another title', description='Freds content'
         )
 
     def test_can_retrieve_drawer_using_valid_id(self):
@@ -48,13 +48,13 @@ class DrawerDetailViewTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_user_can_update_own_drawer(self):
-        self.client.login(username='adam', password='pass')
-        response = self.client.put('/drawers/1/', {'title': 'a new title'})
+        self.client.login(username='chris', password='pass')
+        response = self.client.put('/drawers/1/', {'title': 'asdf'})
         drawer = Drawer.objects.filter(pk=1).first()
         self.assertEqual(drawer.title, 'a new title')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_user_cant_update_another_users_post(self):
-        self.client.login(username='adam', password='pass')
-        response = self.client.put('/drawers/2/', {'title': 'a new title'})
+    def test_user_cant_update_another_users_drawer(self):
+        self.client.login(username='chris', password='pass')
+        response = self.client.put('/drawers/2/', {'title': 'asdf'})
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
